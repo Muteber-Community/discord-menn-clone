5d4dd2ee48a16891792360e5911a4e3b4d1d7192
Mehmet Nail Mutlu <54910442+mehmetnail0@users.noreply.github.com>
2021-05-31 17:43:07 +0300

    deleted updated

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+  
           <code className={styles.code}>pages/index.js</code>
         </p>
 
