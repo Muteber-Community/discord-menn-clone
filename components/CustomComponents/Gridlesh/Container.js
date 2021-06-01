f6239449519b0b34d7d9a4312cdb19ae4b6e23c5
Mehmet Nail Mutlu <54910442+mehmetnail0@users.noreply.github.com>
2021-06-01 20:21:55 +0300

    now we have distinctive container component

---
+++ components/CustomComponents/Gridlesh/Container.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+import styles from '../../../styles/Gridlesh.module.css';
+
+const Container = ({ children, mode, ...rest }) => {
+  const containerStyles = classNames({
+    [styles[mode]]: mode,
+  });
+
+  return (
+    <div {...rest} className={containerStyles}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
+
+Container.propTypes = {
+  mode: PropTypes.oneOf(['normal', 'app-style']),
+};
+Container.defaultProps = {
+  mode: 'normal',
+};
