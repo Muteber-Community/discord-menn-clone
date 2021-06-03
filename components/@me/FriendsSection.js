e5b658356803f1cfa097741e8b2bdacc07a81c28
AhmetYasin07 <75330445+AhmetYasin07@users.noreply.github.com>
2021-06-03 20:13:03 +0300

    Update FriendsSection.js

--- components/@me/FriendsSection.js
+++ components/@me/FriendsSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FriendsSection() {
   return (
     <div className='icons-section'>
-      <div className='icons-section'>Friends All Online Offline</div>
+      <div>Friends All Online Offline</div>
     </div>
   );
 }
