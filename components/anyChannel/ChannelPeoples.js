11329d42b715ebe9672a302c1578347f42cae68a
Mehmet Nail Mutlu <54910442+mehmetnail0@users.noreply.github.com>
2021-06-04 19:21:21 +0300

    chat form created, styling but not ended yet

--- components/anyChannel/ChannelPeoples.js
+++ components/anyChannel/ChannelPeoples.js
@@ -14,7 +14,18 @@ function ChannelPeoples() {
             height='32'
             aria-hidden='true'
           />
-          <span className='peopleList-activity' />
+          <span className='peopleList-activity'>
+            <svg
+              className='avatar-status'
+              width='10'
+              height='10'
+              viewBox='0 0 23 23'
+              x='22'
+              y='22'
+            >
+              <circle cx='11.5' cy='11.5' r='11.5' fill='#399F58' />
+            </svg>
+          </span>
           <span className='peopleList-nameOfPeople'>Mehmet Nail Mutlu</span>
         </div>
         <div className='peopleList-user'>
@@ -26,20 +37,18 @@ function ChannelPeoples() {
             height='32'
             aria-hidden='true'
           />
-          <span className='peopleList-activity' />
-          <span className='peopleList-nameOfPeople'>Ahmet Yasin</span>
-        </div>
-        <p className='peopleList-roleTitle'>Javascipt classes</p>
-        <div className='peopleList-user'>
-          <img
-            className='peopleList-image'
-            src='https://cdn.discordapp.com/icons/678433623266820118/be363953a60f6c52bd358a0615072940.png?size=256'
-            alt=''
-            width='32'
-            height='32'
-            aria-hidden='true'
-          />
-          <span className='peopleList-activity' />
+          <span className='peopleList-activity'>
+            <svg
+              className='avatar-status'
+              width='10'
+              height='10'
+              viewBox='0 0 23 23'
+              x='22'
+              y='22'
+            >
+              <circle cx='11.5' cy='11.5' r='11.5' fill='#399F58' />
+            </svg>
+          </span>{' '}
           <span className='peopleList-nameOfPeople'>Ahmet Yasin</span>
         </div>
       </div>
--- components/anyChannel/ChatArea.js
+++ components/anyChannel/ChatArea.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function ChatArea() {
-  return <div className='peopleList-left'>USERSLEFTList</div>;
+  return (
+    <div className='peopleList-left'>
+      <div className='peopleList-messages'>Mesaj 1</div>
+
+      <div className='peopleList-typeMessage'>
+        <div className='peopleList-iconsLeft'></div>
+        <form className='peopleList-chatForm'>
+          <input type='text' placeholder='#chat Kanalına Mesaj Gönder' />
+        </form>
+        <div className='peopleList-iconsRight'></div>
+      </div>
+    </div>
+  );
 }
 
 export default ChatArea;
